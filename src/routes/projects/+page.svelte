49197fb7a27c741d4fedfaa361c18d371bd57407
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2023-10-24 10:46:59 +0200

    Added projects route
    Changelog:
    - Added projects route
    - Changed button size

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,14 @@
+<section>
+    <article>
+        <h1>Projects</h1>
+        <p>These are my best projects.</p>
+    </article>
+</section>
+
+<style>
+  article {
+    margin: auto;
+    width: 80%;
+    /* border: 3px solid var(--c-error); */
+  }
+</style>
