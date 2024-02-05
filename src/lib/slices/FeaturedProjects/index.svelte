1e33a3ef86ddb7dd5c248591a33b211ab6da8cfc
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2024-02-05 22:03:32 +0100

    Project styling
    Changelog:
    - Project styling (list)

--- src/lib/slices/FeaturedProjects/index.svelte
+++ src/lib/slices/FeaturedProjects/index.svelte
@@ -14,6 +14,7 @@
       {#each slice.items as item}
         <a href="projects/{item.project_link}">
           <li>
+            <img src={item.project_img.url} alt={item.project_title} />
             <h3>{item.project_title}</h3>
             <span>{item.project_description}</span>
           </li>
@@ -53,9 +54,17 @@
   li {
     list-style: none;
     padding: 2rem;
-    margin-bottom: 1rem;
+    margin-bottom: 2rem;
     border-radius: var(--border-radius);
     border: 1px solid rgb(0, 0, 0);
+  }
+
+  img {
+    display: block;
+    margin: auto;
+    width: auto;
+    height: 10rem;
+    margin-bottom: 2rem;
   }
 
   h3 {
@@ -68,24 +77,34 @@
     color: var(--c-text);
   }
 
-  @media only screen and (min-width: 767px) {
+  @media only screen and (min-width: 1067px) {
     section {
       margin: 6rem 4rem 4rem 4rem;
       display: grid;
-      grid-template-columns: repeat(2, 1fr);
+      grid-template-columns: repeat(1, 1fr);
       grid-template-rows: 1fr;
       gap: 0;
       grid-template-areas: "text projects";
     }
 
     h2 {
-      font-size: 3rem;
+      font-size: 5rem;
+      margin-bottom: 1rem;
     }
 
     p {
       margin: 0 0 16rem 0;
       font-size: clamp(1.8rem, 5.5vw, 2.125rem);
       width: 80%;
+    }
+
+    li {
+      border-width: 2px;
+      margin-bottom: 3rem;
+    }
+
+    img {
+      height: 20rem;
     }
   }
 </style>
